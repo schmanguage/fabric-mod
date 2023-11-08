49f3d6d5c768e060e7b1b37086a6b80acec2211c
Tomate 06 13 <69756782+Tomate0613@users.noreply.github.com>
2023-11-08 14:28:55 +0100

    Fix some bugs

---
+++ src/main/java/com/tomate/schmoahify/Schmanguage.java
@@ -0,0 +1,51 @@
+package com.tomate.schmoahify;
+
+import java.util.Arrays;
+
+public class Schmanguage {
+    public static String translate(String text) {
+        if(text.trim().length() < 2)
+            return text;
+
+        var words = text.split(" ");
+
+        var modifiedWords = Arrays.stream(words).map((word) -> {
+            if (word.length() < 2)
+                return word;
+
+            var uppercaseWord = word.toUpperCase();
+            var lowercaseWord = word.toLowerCase();
+
+            // Check if the word is an actual word
+            if(uppercaseWord.equals(lowercaseWord))
+                return word;
+
+            var isUppercase = word.equals(uppercaseWord);
+
+            // Is the first letter uppercase?
+            if (uppercaseWord.charAt(0) != word.charAt(0))
+                return word;
+
+            var modifiedWord = word;
+            while(!startsWithVowel(modifiedWord) && modifiedWord.length() > 2) {
+                modifiedWord = modifiedWord.substring(1);
+            }
+
+            if(modifiedWord.length() == 2)
+                return word;
+
+            if(isUppercase)
+                return "SCHM" + modifiedWord;
+
+            String modifiedWordWithFirstLetterLowercase = Character.toLowerCase(modifiedWord.charAt(0)) + modifiedWord.substring(1);
+            return "Schm" + modifiedWordWithFirstLetterLowercase;
+        });
+
+        // Join words together again
+        return modifiedWords.reduce((word1, word2) -> word1 + " " + word2).orElse("");
+    }
+
+    private static boolean startsWithVowel(String string) {
+        return "AEIOUaeiou".indexOf(string.charAt(0)) != -1;
+    }
+}
--- src/main/java/com/tomate/schmoahify/Schmoahify.java
+++
@@ -1,9 +0,0 @@
-package com.tomate.schmoahify;
-
-import net.fabricmc.api.ModInitializer;
-
-public class Schmoahify implements ModInitializer {
-    @Override
-    public void onInitialize() {
-    }
-}
