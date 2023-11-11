f8696adb2e975d4024c75c9325026416d59b427a
Tomate 06 13 <69756782+Tomate0613@users.noreply.github.com>
2023-11-11 12:22:40 +0100

    Automatically translate all untranslated TranslatableTexts

---
+++ src/main/java/net/schmanguage/mixin/ClientLanguageMixin.java
@@ -0,0 +1,40 @@
+package net.schmanguage.mixin;
+
+import net.minecraft.client.resources.language.ClientLanguage;
+import net.minecraft.locale.Language;
+import net.minecraft.server.packs.resources.Resource;
+import net.schmanguage.Schmanguage;
+import org.slf4j.Logger;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+import java.util.Map;
+
+@Mixin(ClientLanguage.class)
+public abstract class ClientLanguageMixin {
+    @Shadow @Final private static Logger LOGGER;
+
+    @Redirect(method = "loadFrom", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/resources/language/ClientLanguage;appendFrom(Ljava/lang/String;Ljava/util/List;Ljava/util/Map;)V"))
+    private static void appendFrom(String languageIdentifier, List<Resource> resourceStack, Map<String, String> map) {
+        for (Resource resource : resourceStack) {
+            try {
+                try (InputStream inputStream = resource.open()) {
+                    Language.loadFromJson(inputStream, (key, value) -> {
+                        if(languageIdentifier.equals("en_schm"))
+                            Schmanguage.languageKeys.add(key);
+
+                        map.put(key, value);
+                    });
+                }
+            } catch (IOException iOException) {
+                LOGGER.warn("Failed to load translations for {} from pack {}", languageIdentifier, resource.sourcePackId(), iOException);
+            }
+        }
+    }
+}
---
+++ src/main/java/net/schmanguage/mixin/TranslatableContentsMixin.java
@@ -0,0 +1,24 @@
+package net.schmanguage.mixin;
+
+import net.minecraft.network.chat.contents.TranslatableContents;
+import net.schmanguage.Schmanguage;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+
+@Mixin(TranslatableContents.class)
+public abstract class TranslatableContentsMixin {
+    @Shadow @Final private String key;
+
+
+    @ModifyVariable(method = "decompose", at = @At(value = "STORE", ordinal = 0), ordinal = 0)
+    String translate(String string) {
+        if(Schmanguage.languageKeys.contains(this.key))
+            return string;
+
+        return Schmanguage.translate(string);
+    }
+}
